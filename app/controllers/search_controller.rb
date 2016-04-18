1f3de9184cdc7d1a4570ca055ce6c55470568d55
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-18 10:11:33 -0600

    parsing data returned from the API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   def index
-    @stations = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params['q']}&radius=6.0&limit=10&api_key=ENV['NREL_API_KEY']&format=JSON")
-    binding.pry
+    @stations = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params['q']}&radius=6.0&limit=10&api_key=ugX7KtCPuI58nDMXUUnLHNpLlnTalck4zwShcRlk&format=JSON")
+binding.pry
+    redirect_to root_path
+    # https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=ugX7KtCPuI58nDMXUUnLHNpLlnTalck4zwShcRlk&format=JSON
+    # binding.pry
   end
 end
--- spec/features/user_can_search_by_zip_code_spec.rb
+++ spec/features/user_can_search_by_zip_code_spec.rb
@@ -7,7 +7,9 @@ RSpec.feature "user can enter in zip code" do
     fill_in "q", with: "80203"
     click_on "Locate"
     expect(current_path).to eq "/search?zip=80203"
-    expect(page).to have_content ""
+    expect(page).to have_content "CADACHARGEPOINT"
+    expect(page).to have_content "290 E Speer Blvd Denver, CO 80203"
+    expect(page).to have_content "24 hours daily"
   end
 end
 
