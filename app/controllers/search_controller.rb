e3b7d4a32842f762e184e0da2193bb2ed4442c62
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-18 11:48:39 -0600

    Interpolate ENV for api request call
    I wasn't thinking straight and forgot to interpolate the environment variable
    This is very embarrassing..

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @stations = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params['q']}&radius=6.0&limit=10&api_key=ENV['NREL_API_KEY']&format=JSON")
+    @stations = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params['q']}&radius=6.0&limit=10&api_key=#{ENV['NREL_API_KEY']}&format=JSON")
   end
 end
