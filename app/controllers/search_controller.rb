6790d72d1d8f0c7af7d8153c7885f75066129877
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-18 09:58:15 -0600

    trying to return alternative fuel stations from the API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @stations = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=ENV['NREL_API_KEY']&zip=#{params['q']}&radius=6.0")
-
+    @stations = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params['q']}&radius=6.0&limit=10&api_key=ENV['NREL_API_KEY']&format=JSON")
     binding.pry
   end
 end
